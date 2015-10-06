5620bdfa01750a7a29b0a9eb308c9d9d58494757
Ignacio Nieto <contact@digitalleaves.com>
2015-10-06 11:53:37 +0200

    Initial Commit

---
+++ CryptoLoadExternalCertificate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoLoadExternalCertificate
+//
+//  Created by Ignacio Nieto Carvajal on 6/10/15.
+//  Copyright © 2015 Ignacio Nieto Carvajal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
